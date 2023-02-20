cec8a6dff3bc7520b35f52062932fdcfffa9338c
Dirk Kretschmann <kretschmanndi@gmail.com>
2023-02-21 00:06:22 +0100

    add simple file reader and parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+use regex::Regex;
 use reqwest::Response;
 
 pub struct HttpRequest {
-    pub method: String,
-    pub url: String,
+    pub method: Option<String>,
+    pub url: Option<String>,
+}
+
+impl HttpRequest {
+    pub fn new() -> Self {
+        Self {
+            method: None,
+            url: None,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -20,26 +34,40 @@ impl HttpResponse {
     }
 }
 
-pub fn parse() -> HttpRequest {
-    HttpRequest {
-        method: "GET".to_string(),
-        url: "https://httpbin.org/ip".to_string(),
+pub fn parse() -> Result<HttpRequest, Box<dyn std::error::Error>> {
+    let file = File::open("tests/collection/simple_get.http")?;
+    let lines = BufReader::new(file).lines();
+
+    let mut http_req = HttpRequest::new();
+    let re = Regex::new(r"\s*(\S*)\s*(\S*)").unwrap();
+
+    for line in lines {
+        let line = line.unwrap();
+
+        if let Some(cap) = re.captures_iter(&line).next() {
+            http_req.method = Some(cap[1].to_string());
+            http_req.url = Some(cap[2].to_string());
+        }
     }
+
+    Ok(http_req)
 }
 
-pub async fn request(http_req: HttpRequest) -> Result<HttpResponse, Box<dyn std::error::Error>>{
-    let res = reqwest::get("https://httpbin.org/ip").await?;
+pub async fn request(http_req: HttpRequest) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+    let method = reqwest::Method::from_bytes(http_req.method.unwrap().as_bytes())?;
+    let url = http_req.url.unwrap();
+
+    let client = reqwest::Client::new();
+    let res = client.request(method, url).send().await?;
+
     let http_res = HttpResponse::from(res).await?;
 
     Ok(http_res)
 }
 
-
-
 pub async fn execute() -> Result<(), Box<dyn std::error::Error>> {
-    let res = reqwest::get("https://httpbin.org/ip").await?;
-
-    let http_res = HttpResponse::from(res).await?;
+    let http_req = parse()?;
+    let http_res = request(http_req).await?;
 
     println!("{:?}", http_res);
 
